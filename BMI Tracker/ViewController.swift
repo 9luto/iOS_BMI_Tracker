c22d2eb361774023441e4ae462435f00a3271950
Richard Peralta <peraltar91@my.sunyulster.edu>
2020-05-09 21:47:17 -0400

    Initial Commit

---
+++ BMI Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Tracker
+//
+//  Created by Richard Peralta on 5/9/20.
+//  Copyright © 2020 Richard Peralta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
